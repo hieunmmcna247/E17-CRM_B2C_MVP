7db32654693ad724659f1867203b936bda9cb5be
hieunmmcna247 <hieunm.mcna.247@gmail.com>
2026-05-18 15:54:13 +0700

    wip: notification updates

--- src/hooks/use-notifications.ts
+++ src/hooks/use-notifications.ts
@@ -47,7 +47,7 @@ export function useNotifications() {
     // Polling don gian thay cho Realtime de tranh loi subscribe
     const interval = setInterval(() => {
       void fetchNotifications()
-    }, 10000) // refresh moi 30 giay
+    }, 10000) // refresh moi 10 giay
 
     return () => {
       clearInterval(interval)
